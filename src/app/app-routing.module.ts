6fbcee89a29404eb313703d53f556f5b231a64ae
NotAUserName0 <dill1@live.com.mx>
2022-11-18 17:16:52 -0600

    add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConfiguracionComponent } from './components/configuracion/configuracion.component';
+import { EntradasComponent } from './components/entradas/entradas.component';
+import { MainComponent } from './components/main/main.component';
+import { MensajesComponent } from './components/mensajes/mensajes.component';
+import { NoPageFoundComponent } from './components/no-page-found/no-page-found.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo : 'home', pathMatch : 'full'},
+  {path:'home', component : MainComponent},
+  {path:'entradas', component : EntradasComponent},
+  {path:'mensajes', component : MensajesComponent},
+  {path:'perfil', component: PerfilComponent},
+  {path: 'configuracion', component: ConfiguracionComponent},
+  {path:'**', component: NoPageFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { MensajesComponent } from './components/mensajes/mensajes.component';
+import { EntradasComponent } from './components/entradas/entradas.component';
+import { NoPageFoundComponent } from './components/no-page-found/no-page-found.component';
+import { ConfiguracionComponent } from './components/configuracion/configuracion.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    NavbarComponent,
+    PerfilComponent,
+    MensajesComponent,
+    EntradasComponent,
+    NoPageFoundComponent,
+    ConfiguracionComponent
   ],
   imports: [
     BrowserModule,
